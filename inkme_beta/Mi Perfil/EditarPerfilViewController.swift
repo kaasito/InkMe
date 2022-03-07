b44253ee5305bf4de08ee2549b1327204d8d4791
Lucas Romero Magaña <lucasromeromagana@gmail.com>
2022-03-07 14:01:47 +0100

    cambiada interfaz, y foto de perfil se coge correctamente

---
+++ inkme_beta/Mi Perfil/EditarPerfilViewController.swift
@@ -0,0 +1,66 @@
+//
+//  EditarPerfilViewController.swift
+//  inkme_beta
+//
+//  Created by Lucas Romero Magaña on 7/3/22.
+//
+
+import UIKit
+import Alamofire
+import AlamofireImage
+
+class EditarPerfilViewController: UIViewController {
+    
+    @IBOutlet weak var telefonoField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var nombreField: UITextField!
+    @IBOutlet weak var editarImagenButton: UIButton!
+    @IBOutlet weak var imagenPerfil: UIImageView!
+    let defaults = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imagenPerfil.layer.cornerRadius = imagenPerfil.frame.size.width / 2
+        imagenPerfil.clipsToBounds = true
+        let url = "http://desarrolladorapp.com/inkme/public/api/cargarPerfil"
+        let idUsuario = defaults.integer(forKey: "id")
+        let json = ["usuario_id": String(idUsuario)]
+        AF.request(url, method: .put, parameters: json, encoding: JSONEncoding.default).responseDecodable (of: PerfilResponseEditar.self) { [self] response in
+            print("response",response)
+            if (response.value?.status) == 1 {
+                imagenPerfil.af.setImage(withURL: URL(string: (response.value?.usuario?.foto ??  "https://fundaciongaem.org/wp-content/uploads/2016/05/no-foto.jpg") )! )
+                
+            }else{
+                print("no se ha podido hacer fetch")
+            }
+        }
+    }
+    
+    
+    @IBAction func editarImagenTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func guardarCambiosTapped(_ sender: Any) {
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+    
+    
+}
+
+struct PerfilResponseEditar:Decodable {
+    let status: Int?
+    let usuario: UsuarioPropioEditar?
+}
+
+struct UsuarioPropioEditar:Decodable{
+ 
+    let nombre: String?
+    let email: String?
+    let foto: String?
+    let ubicacion: String?
+    let styles: String?
+}
