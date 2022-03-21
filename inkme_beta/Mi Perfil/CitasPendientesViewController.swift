cf1241422b9fc8f48986c4ab5912c1189a11378f
Lucas Romero Magaña <lucasromeromagana@gmail.com>
2022-03-22 00:23:15 +0100

    faltaba controller

---
+++ inkme_beta/Mi Perfil/CitasPendientesViewController.swift
@@ -0,0 +1,65 @@
+//
+//  CitasPendientesTableViewController.swift
+//  inkme_beta
+//
+//  Created by Lucas Romero Magaña on 21/3/22.
+//
+
+import UIKit
+
+class CitasPendientesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
+
+    var citas: [CitasPendientes]?
+    @IBOutlet weak var tabla: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabla.delegate = self
+        self.tabla.dataSource = self
+       
+        PendientesNetWorking.shared.getUser() { arrayCitas in
+            self.citas = arrayCitas
+            self.tabla.reloadData()
+        } failure: { error in
+            print(error)
+        }
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.tabla.delegate = self
+        self.tabla.dataSource = self
+        PendientesNetWorking.shared.getUser() { arrayCitas in
+            self.citas = arrayCitas
+            self.tabla.reloadData()
+        } failure: { error in
+            print(error)
+        }
+        
+        self.tabla.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return citas?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PendientesCell", for: indexPath) as! CitasPendientesTableViewCell
+        cell.nombre.text = citas![indexPath.row].client_name
+        cell.fecha.text = citas![indexPath.row].date
+        cell.telefono.text = citas![indexPath.row].client_tlf
+        cell.mensaje.text = citas![indexPath.row].comment
+        cell.id = citas![indexPath.row].id
+        cell.backgroundColor = .clear
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let cell = tableView.cellForRow(at: indexPath) as! CitasPendientesTableViewCell
+        cell.backgroundColor = .clear
+        let bgColorView = UIView()
+        bgColorView.backgroundColor = UIColor.black
+        cell.selectedBackgroundView = bgColorView
+    }
+}
+
